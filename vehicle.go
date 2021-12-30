3735b8655892271743c053724b6c4ddf868b2d5e
Bec Sun <Bec.sun@aexp.com>
2021-12-30 14:20:14 +0000

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -6,10 +6,44 @@ import (
 
 
 // Values array for the feedback.json file
-
+type Values struct{
+ Models []Model `json:"values"`
+}
 // Model array for the feedback.json file
+type Model struct{
+	Name string `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct{
+	feedbackTotal int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral int
+}
+
+type vehicle interface{
+}
+
+type car struct{
+	model string
+	make string
+	typeVehicle string
+}
+
+type truck struct{
+	model string
+	make string
+	typeVehicle string
+}
+
+type bike struct{
+	model string
+	make string
+}
 
 type rating float32
+
 
 const (
 	extraPositive rating = 1.2
@@ -19,9 +53,14 @@ const (
 	extraNegative rating = -1.2
 )
 
+
+var inventory []vehicle
+
+var vehicleResult map[string]feedbackResult
+
 func init() {
 
-/*
+
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,8 +73,8 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
